00947bc377023ebadabf1445ab6d2604352b7839
Alex Kaufman and Matthew Terrill <meterrill@gmail.com>
2015-04-01 15:39:01 -0700

    Add messages table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150401173237) do
+ActiveRecord::Schema.define(version: 20150401175335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "to"
+    t.string   "from"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
